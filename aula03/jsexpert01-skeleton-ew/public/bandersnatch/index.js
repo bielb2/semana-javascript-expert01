4dbc6a1393c727b828d5975d9aa203002e3f616c
bielb2 <bielb2@hotmail.com>
2020-10-29 21:19:04 -0300

    update folder and add http ; aula 03

---
+++ aula03/jsexpert01-skeleton-ew/public/bandersnatch/index.js
@@ -0,0 +1,20 @@
+const MANIFEST_URL = 'manifest.json'
+const localHost = ['127.0.0.1', 'localhost']
+
+async function main() {
+    const isLocal = !!~localHost.indexOf(window.location.hostname)
+    console.log('dsa', isLocal);
+    const manifestJSON = await (await (await fetch(MANIFEST_URL)).json())
+    const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
+    const videoComponent = new VideoComponent()
+    
+    const videoPlayer = new VideoMediaPlayer({
+        manifestJSON
+    })
+
+    videoPlayer.initializeCodec()
+    videoComponent.initializePlayer()
+
+}
+
+window.onload = main
